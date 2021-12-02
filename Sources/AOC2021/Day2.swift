a297140aa098f7b8060b41304c8ea69f4b9c410a
Julian Kahnert <mail@juliankahnert.de>
2021-12-02 13:32:03 +0100

    day 2

--- Sources/AOC2021/Day2.swift
+++ Sources/AOC2021/Day2.swift
@@ -8,19 +8,71 @@
 import Foundation
 import AOCHelper
 
-// typealias DepthCount = (lastDepth: Int, count: Int)
-//
-// func numOfIncreasedDepth(_ values: [Int], windowSize: Int = 1) -> Int {
-//
-//    let depthCount: DepthCount = values.indices
-//        .reduce(into: (lastDepth: Int.max, count: 0)) { tempDepthCount, index in
-//            guard let window = values.window(from: index, size: windowSize) else { return }
-//            let currentDepth = window.reduce(0, +)
-//            if tempDepthCount.lastDepth < currentDepth {
-//                tempDepthCount.count += 1
-//            }
-//            tempDepthCount.lastDepth = currentDepth
-//        }
-//
-//    return depthCount.count
-// }
+struct SteeringCommand {
+    let direction: Direction
+    let value: Int
+}
+
+enum Direction: String {
+    case forward, down, up
+}
+
+func calculateSubmarineRoute(from inputs: [String], isPart1: Bool = true) -> Point3 {
+    var position: Point3 = .zero
+    for input in inputs {
+        let command = parseSubmarineInput(input)
+        
+        if isPart1 {
+            position = position.apply(command)
+        } else {
+            position = position.applyPart2(command)
+        }
+    }
+    return position
+}
+
+private func parseSubmarineInput(_ input: String) -> SteeringCommand {
+    let rawInput = input.trimmingCharacters(in: .whitespacesAndNewlines)
+        .components(separatedBy: " ")
+    
+    assert(rawInput.count == 2)
+    
+    guard let direction = Direction(rawValue: rawInput[0]),
+          let value = Int(rawInput[1]) else { fatalError("Failed to parse command input \(input)") }
+    
+    return SteeringCommand(direction: direction, value: value)
+}
+
+extension Point3 {
+    fileprivate func apply(_ command: SteeringCommand) -> Point3 {
+        switch command.direction {
+        case .forward:
+            return Point3(x: self.x, y: self.y, z: self.z + command.value)
+        case .down:
+            return Point3(x: self.x, y: self.y - command.value, z: self.z)
+        case .up:
+            return Point3(x: self.x, y: self.y + command.value, z: self.z)
+        }
+    }
+    
+    fileprivate func applyPart2(_ command: SteeringCommand) -> Point3 {
+        switch command.direction {
+        case .forward:
+            return Point3(x: self.x, y: self.y - (self.x * command.value), z: self.z + command.value)
+        case .down:
+            return Point3(x: self.x + command.value, y: self.y, z: self.z)
+        case .up:
+            return Point3(x: self.x - command.value, y: self.y, z: self.z)
+        }
+    }
+    
+    var depth: Int {
+        -1 * y
+    }
+    var horizontal: Int {
+        z
+    }
+    var aim: Int {
+        x
+    }
+}
